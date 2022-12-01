c953b5f0d64d61e27d06ec13b204ae4500ebce7a
roy <kimathiwaweru@gmail.com>
2022-12-01 10:41:16 +0300

    assignment done

---
+++ index.js
@@ -0,0 +1,14 @@
+function divide (){
+    let num1=2000;
+    let num2=100;
+    return num1/num2;
+}
+function square(){
+    let x=2;
+    return x**2;
+}
+function add(){
+    let a=3;
+    let b=4;
+    return a+b
+}
